b38a915cc4988b2773a72bd44e836371212bcb4c
Elena Vargas <elenavargasm9@gmail.com>
2023-01-23 18:23:03 +0100

    empty

--- src/components/Hompage.js
+++ src/components/Hompage.js
@@ -4,7 +4,7 @@ function Homepage() {
   return (
     <article className="home">
       <h2 className="home__title">
-        Hello! I am Elena, <span className="span">web developer.</span>
+        Hello! I am Elena, <span className="span">web developer!.</span>
       </h2>
       <img className="home__avatar" src={avatar} alt="avatar" />
     </article>
