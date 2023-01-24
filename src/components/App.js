88514590b640e13d28d649f9ac36c6e1aa7c08e0
Elenavar <elenavargasm9@gmail.com>
2023-01-24 23:12:42 +0100

    test 2

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import Projects from './Projects';
 function App() {
   return (
     <div className="page">
-      {/* <Header /> */}
+      <Header />
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/projects" element={<Projects />}></Route>
